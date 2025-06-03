80b1c3507adf554be3a6a48af1efaa6cd380b935
m2635 <marlontg@proton.me>
2025-06-03 16:57:52 +0200

    browse by tag function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import Link from 'next/link';
-import React from 'react';
-import HeroSection from "@/components/hero";
-import PostList from "@/components/postlist";
+import Link from 'next/link'
+import React from 'react'
+import HeroSection from '@/components/hero'
+import PostList from '@/components/postlist'
+import TagButtonGrid from '@/components/tagsection'
+import { getAllPosts } from '@/lib/posts'
 
 export default function HomePage() {
+    const posts = getAllPosts()
+
+    const tagCounts: Record<string, number> = {}
+    posts.forEach(post => {
+        post.tags?.forEach(tag => {
+            tagCounts[tag] = (tagCounts[tag] || 0) + 1
+        })
+    })
 
     return (
         <main className="mx-auto w-5/6">
             <HeroSection />
+            <TagButtonGrid tagCounts={tagCounts} />
             <PostList />
         </main>
-    );
+    )
 }
---
+++ src/components/tagsection.tsx
@@ -0,0 +1,39 @@
+import { slug } from 'github-slugger'
+import Link from 'next/link'
+import { Hash } from 'lucide-react'
+type TagButtonGridProps = {
+    tagCounts: Record<string, number>
+}
+
+export default function TagButtonGrid({ tagCounts }: TagButtonGridProps) {
+    const sortedTags = Object.keys(tagCounts).sort(
+        (a, b) => tagCounts[b] - tagCounts[a]
+    )
+
+    return (
+        <section className="w-full bg-gray-100 rounded-xl flex flex-col items-center justify-center py-20 px-4">
+            <h2 className="text-3xl md:text-4xl font-bold text-black mb-3 text-center">
+                Search by Tags
+            </h2>
+            <p className="text-gray-600 dark:text-gray-600 mb-12 text-center max-w-2xl">
+                Explore our diverse array of articles using tags.
+            </p>
+
+            <div className="flex flex-wrap justify-center gap-6 max-w-5xl">
+                {sortedTags.map((tag) => (
+                    <Link
+                        key={tag}
+                        href={`/tags/${slug(tag)}`}
+                        className="flex items-center gap-3 px-6 py-4 text-lg border-2 border-gray-300 dark:border-gray-600 rounded-xl bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-700 transition"
+                    >
+                        <Hash className="w-6 h-6" />
+                        <span className="capitalize font-medium">{tag}</span>
+                        <span className="text-base text-gray-500 dark:text-gray-400">
+              ({tagCounts[tag]})
+            </span>
+                    </Link>
+                ))}
+            </div>
+        </section>
+    )
+}
